b021749075658d8162f9a2e2509cae8dd5069ae8
Jon Peterson <jon@pnwebs.com>
2019-02-13 09:32:23 -0700

    Add add_book method to Author.

--- lib/author.rb
+++ lib/author.rb
@@ -2,7 +2,17 @@ class Author
   attr_reader :books
 
   def initialize(details)
-    @details = details
+    @first_name = details[:first_name]
+    @last_name = details[:last_name]
     @books = []
+  end
+
+  def add_book(title, publication_date)
+    @books << Book.new({
+      author_first_name: @first_name,
+      author_last_name: @last_name,
+      title: title,
+      publication_date: publication_date
+    })
   end
 end
