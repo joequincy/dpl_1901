bf10d8901258615634cadb50ebe2efa01cffd071
Jon Peterson <jon@pnwebs.com>
2019-02-13 10:10:41 -0700

    Add find_by_author method.

--- lib/library.rb
+++ lib/library.rb
@@ -16,4 +16,14 @@ class Library
   def card_catalog
     @books.sort_by{|book| book.author_last_name}
   end
+
+  def find_by_author(full_name)
+    authored_books = {}
+    @books.each do |book|
+      if full_name == "#{book.author_first_name} #{book.author_last_name}"
+        authored_books[book.title] = book
+      end
+    end
+    return authored_books
+  end
 end
