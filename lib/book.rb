b033fe0e091fdcb599cdab088579cccd8ac06f25
Jon Peterson <jon@pnwebs.com>
2019-02-13 09:16:14 -0700

    Add attributes and attr_readers to Book.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,13 @@
 class Book
+  attr_reader :author_first_name,
+              :author_last_name,
+              :title
+
   def initialize(details)
-    @details = details
+    @author_first_name = details[:author_first_name]
+    @author_last_name = details[:author_last_name]
+    @title = details[:title]
+    @publication_date = details[:publication_date]
   end
 
 end
