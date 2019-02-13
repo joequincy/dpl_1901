cf49738a70ed93aee1ce9ece09c70cc26bd0dcd5
Jon Peterson <jon@pnwebs.com>
2019-02-13 09:17:23 -0700

    Add test for publication_date returning year.

--- test/book_test.rb
+++ test/book_test.rb
@@ -14,4 +14,8 @@ class BookTest < Minitest::Test
     assert_equal "Lee", @book.author_last_name
     assert_equal "To Kill a Mockingbird", @book.title
   end
+
+  def test_book_returns_year_for_publication_date
+    assert_equal "1960", @book.publication_date
+  end
 end
